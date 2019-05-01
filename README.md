4ba1662e54986636149c9c50c16e2b3ca9c052cf
Rockheung <rockheung@gmail.com>
2019-05-02 01:32:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-x2xtyf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-x2xtyf)
---
+++ index.js
@@ -0,0 +1,4 @@
+let canvas = document.querySelector("#root")
+let context = canvas.getContext('2d')
+
+context.strokeRect(10,15,50,60)
