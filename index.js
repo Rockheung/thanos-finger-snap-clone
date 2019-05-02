cc5be16b6168f1731086bb01f201452ffa331f11
Rockheung <rockheung@gmail.com>
2019-05-02 17:05:17 +0900

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 let canvas = document.querySelector("#root")
-let context = canvas.getContext('2d')
-
-context.strokeRect(10,15,50,60)
+let ctx = canvas.getContext('2d')
+console.log('sdgsdf')
+var hand = new Image();
+hand.src = 'static/thanos_idle.png'
+hand.onload = ()=>{
+  ctx.drawImage(hand,0,0,hand.width, hand.height)
+  console.log('Complete')
+}
+hand.onerror=(e)=>{
+  console.log(e)
+}
+ctx.strokeRect(10,15,50,60)
