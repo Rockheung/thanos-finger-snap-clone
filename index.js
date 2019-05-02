07fffbad3500609a7643aa03a8182e4685441e15
Rockheung <rockheung@gmail.com>
2019-05-02 17:06:20 +0900

    3 files modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let canvas = document.querySelector("#root")
 let ctx = canvas.getContext('2d')
 console.log('sdgsdf')
 var hand = new Image();
-hand.src = 'static/thanos_idle.png'
+hand.src = 'thanos_idle.png'
 hand.onload = ()=>{
   ctx.drawImage(hand,0,0,hand.width, hand.height)
   console.log('Complete')
