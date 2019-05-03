12abcaab3d35a6d9a40f020c827f858973a770d7
Rockheung <rockheung@gmail.com>
2019-05-03 10:29:24 +0900

    2 files modified

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-let canvas = document.querySelector("#root")
+const imgSize = 80
+const canvasRatio = 2
+const framerate = 15
+let canvas = document.querySelector("#root");
 let ctx = canvas.getContext('2d')
 var handIdle = new Image();
 handIdle.src = 'https://rockheung.github.io/thanos-finger-snap-clone/thanos_idle.png'
 handIdle.onload = ()=>{
-  ctx.drawImage(handIdle,0,0,handIdle.width*5, handIdle.height*5)
+  ctx.drawImage(handIdle,0,0,handIdle.width*canvasRatio, handIdle.height*canvasRatio)
 }
 handIdle.onerror=(e)=>{
   console.log(e)
@@ -13,20 +16,20 @@ const renderSnap = () => {
   let handSnap = new Image();
   handSnap.src = 'https://rockheung.github.io/thanos-finger-snap-clone/thanos_snap.png'
   handSnap.onload=()=>{
-    window.requestAnimationFrame(()=>renderFrame(0,handSnap))
+    renderFrame(0,handSnap)
   }
 }
 
 const renderFrame = (i,img) => {
   if (i>img.width/80) {
-    ctx.clearRect(0,0,400,400)
-    ctx.drawImage(handIdle,0,0,handIdle.width*5, handIdle.height*5)
+    ctx.clearRect(0,0,imgSize*canvasRatio,imgSize*canvasRatio)
+    ctx.drawImage(handIdle,0,0,handIdle.width*canvasRatio, handIdle.height*canvasRatio)
     return
   }
   let newImg = new Image()
   newImg.src = img.src
-  ctx.clearRect(0,0,400,400)
-  ctx.drawImage(img,i*80,0,80,80,0,0,80*5, 80*5)
-  window.requestAnimationFrame(()=>renderFrame(i+1,img))
+  ctx.clearRect(0,0,imgSize*canvasRatio,imgSize*canvasRatio)
+  ctx.drawImage(newImg,i*80,0,80,80,0,0,80*canvasRatio, 80*canvasRatio)
+  setTimeout(()=>renderFrame(i+1,img),1000/framerate)
 }
 canvas.onclick = renderSnap
