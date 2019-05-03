44468a13426470e9597182d8bcf96c8fe3fc9ded
Rockheung <rockheung@gmail.com>
2019-05-03 09:25:39 +0900

    2 files modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let canvas = document.querySelector("#root")
 let ctx = canvas.getContext('2d')
 var handIdle = new Image();
-handIdle.src = 'thanos_idle.png'
+handIdle.src = 'https://rockheung.github.io/thanos-finger-snap-clone/thanos_idle.png'
 handIdle.onload = ()=>{
   ctx.drawImage(handIdle,0,0,handIdle.width*5, handIdle.height*5)
 }
@@ -11,7 +11,7 @@ handIdle.onerror=(e)=>{
 
 const renderSnap = () => {
   let handSnap = new Image();
-  handSnap.src = 'thanos_snap.png'
+  handSnap.src = 'https://rockheung.github.io/thanos-finger-snap-clone/thanos_snap.png'
   handSnap.onload=()=>{
     window.requestAnimationFrame(()=>renderFrame(0,handSnap))
   }
@@ -23,6 +23,8 @@ const renderFrame = (i,img) => {
     ctx.drawImage(handIdle,0,0,handIdle.width*5, handIdle.height*5)
     return
   }
+  let newImg = new Image()
+  newImg.src = img.src
   ctx.clearRect(0,0,400,400)
   ctx.drawImage(img,i*80,0,80,80,0,0,80*5, 80*5)
   window.requestAnimationFrame(()=>renderFrame(i+1,img))
