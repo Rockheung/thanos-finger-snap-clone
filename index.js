51eabcae3cb1e530cd6ada6a727ec40182a00ffe
Rockheung <rockheung@gmail.com>
2019-05-09 19:20:00 +0900

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const imgSize = 80
 const canvasRatio = 2
-const framerate = 15
+const framerate = 50
 const moveXoffset = 0;
 let canvas = document.querySelector("#root");
 let ctx = canvas.getContext('2d')
@@ -19,7 +19,7 @@ const renderSnap = () => {
   handSnap.src = 'https://rockheung.github.io/thanos-finger-snap-clone/thanos_snap.png'
   handSnap.onload=()=>{
     if (isRunning) {
-      renderFrame(0,handSnap)
+      renderFrameSync(0,handSnap)
     }
   }
 }
@@ -36,4 +36,28 @@ const renderFrame = (i,img) => {
   ctx.drawImage(img,i*(imgSize+moveXoffset),0,imgSize,imgSize,0,0,imgSize*canvasRatio, 80*canvasRatio)
   setTimeout(()=>renderFrame(i+1,img),1000/framerate)
 }
+
+const renderFrameSync = async (i,img) => {
+  if (i>img.width/imgSize-1) {
+    ctx.clearRect(0,0,imgSize*canvasRatio,imgSize*canvasRatio)
+    ctx.drawImage(handIdle,0,0,handIdle.width*canvasRatio, handIdle.height*canvasRatio)
+    isRunning = true
+    return
+  }
+  isRunning = false
+  ctx.clearRect(0,0,imgSize*canvasRatio,imgSize*canvasRatio)
+  await new Promise(res=>{
+    let handSnap = new Image();
+    handSnap.src = img.src
+    handSnap.onload=()=>{
+      ctx.drawImage(handSnap,i*(imgSize+moveXoffset),0,imgSize,imgSize,0,0,imgSize*canvasRatio, 80*canvasRatio)
+      setTimeout(()=>{
+        renderFrameSync(i+1,handSnap)
+        res()
+      },1000/framerate)
+    }
+  })
+  
+}
+
 canvas.onclick = renderSnap
